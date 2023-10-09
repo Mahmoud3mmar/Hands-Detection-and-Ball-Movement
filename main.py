701db83e95d5d7c9901bde11e462f42df1e1bb6d
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-10-09 11:18:07 +0300

    (Exit Vedio Capture Loop)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import socket
 # mp_hands = mp.solutions.hands
 
 
-
+#Create a VideoCapture object (cap) to access the webcam.
 cap = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
@@ -17,22 +17,36 @@ mpDraw = mp.solutions.drawing_utils
 
 
 while True:
+    # Read a frame from the webcam using cap.read()
     success, image = cap.read()
+    # Convert the BGR image to RGB format because MediaPipe expects RGB input.
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     results = hands.process(imageRGB)
     # checking whether a hand is detected
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks: # working with each hand
+            # For each landmark (lm) in the hand, extract its coordinates and calculate the center (cx, cy).
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = image.shape
+                # 
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                if id == 20:
-                    cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-
+                # if the landmark ID is 20 (usually a fingertip), draw a filled circle at that location.
+                # if id == 20:
+                #     cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                # Use mpDraw.draw_landmarks to draw landmarks and connections on the image.
                 mpDraw.draw_landmarks(image, handLms, mp_hands.HAND_CONNECTIONS)
                 
     cv2.imshow("Output", image)
-    cv2.waitKey(1)
+
+    # Check for a key press. If 'x' is pressed, exit the loop.
+    key = cv2.waitKey(1)
+    if key == ord('x'):
+        break
+
+# Release the camera and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
 
 
 
