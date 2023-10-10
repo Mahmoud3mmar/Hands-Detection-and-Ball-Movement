370065635f732ff545b85ff43873287960d71fc6
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-10-10 22:43:47 +0300

    (send coordinates using socket)

---
+++ BallMovement/BallMovement/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace BallMovement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CoordinatesTextB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CoordinatesTextB
+            // 
+            this.CoordinatesTextB.Location = new System.Drawing.Point(3, 2);
+            this.CoordinatesTextB.Multiline = true;
+            this.CoordinatesTextB.Name = "CoordinatesTextB";
+            this.CoordinatesTextB.Size = new System.Drawing.Size(454, 425);
+            this.CoordinatesTextB.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CoordinatesTextB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CoordinatesTextB;
+    }
+}
+
